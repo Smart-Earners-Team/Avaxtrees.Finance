ef70b153c873e1d5013ffeb96df70cbb3a1e22d5
Justice Ekemezie <ttebify@gmail.com>
2022-05-05 11:00:08 +0100

    Ui improvements

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,10 @@
-import React, { useCallback, useContext, useEffect, useState } from "react";
+import React, {
+  Fragment,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 import Section from "../components/layouts/Section";
 import ConnectWalletButton from "../components/Buttons/ConnectWalletButton";
 import useActiveWeb3React from "../hooks/useActiveWeb3React";
@@ -220,58 +226,75 @@ const IndexPage = (props: PageProps) => {
           <div className="w-full">
             <div className="rounded-xl p-10 bg-white"></div>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-3 mt-5">
-              <MetricChip label="Contract" value={contractBal} symbol="AVAX" />
-              <MetricChip label="Wallet" value={balance} symbol="AVAX" />
-              <MetricChip label="Your Rice" value={riceBal} symbol="Rice" />
               <MetricChip
-                label="Your Rewards"
-                value={avaxRewards}
-                symbol="AVAX"
+                label="Contract Balance"
+                value={contractBal}
+                symbol="Rice"
               />
               <MetricChip
-                label="Your Rewards"
+                label="Your Compounding Count"
                 value={avaxRewards}
-                symbol="AVAX"
+                symbol="Trxs"
+                borderColorClassName="border-[#57BEEB]"
               />
             </div>
           </div>
           <div className="w-full mb-5">
             {active ? (
-              <div
-                className="bg-[#132803]/90 md:bg-transparent p-3 md:p-0 rounded-xl
+              <Fragment>
+                <div
+                  className="flex flex-col md:flex-row items-center space-y-3 md:space-y-0
+                  md:space-x-3 mb-10"
+                >
+                  <MetricChip
+                    label="Your Planted Trees"
+                    value={riceBal}
+                    symbol="Trees"
+                    borderColorClassName="border-[#B8D525]"
+                  />
+                  <MetricChip
+                    label="Pending Rewards"
+                    value={avaxRewards}
+                    symbol="AVAX"
+                    borderColorClassName="border-[#E8C341]"
+                  />
+                </div>
+                <div
+                  className="bg-[#132803]/90 md:bg-transparent p-3 md:p-0 rounded-xl
                   md:rounded-none"
-              >
-                <TextInput
-                  errorMsg={errorMsg}
-                  onChangeHandler={handleInputChange}
-                  value={amountToPay}
-                  onSubmit={handleCookRice}
-                  trx={cooking}
-                  isDisabled={
-                    cooking ||
-                    errorMsg.length > 0 ||
-                    Number.isNaN(Number.parseFloat(amountToPay))
-                  }
-                />
-                <div className="p-3">
-                  <div className="space-x-3 flex justify-center items-center my-3">
-                    <Button
-                      onClick={handleReCookRice}
-                      disabled={reCooking || !active}
-                      loading={reCooking}
-                    >
-                      Re Plant
-                    </Button>
-                    <Button
-                      onClick={handleEatRice}
-                      disabled={eating || !active}
-                      loading={eating}
-                    >
-                      Harvest
-                    </Button>
+                >
+                  <TextInput
+                    errorMsg={errorMsg}
+                    onChangeHandler={handleInputChange}
+                    value={amountToPay}
+                    onSubmit={handleCookRice}
+                    trx={cooking}
+                    isDisabled={
+                      cooking ||
+                      errorMsg.length > 0 ||
+                      Number.isNaN(Number.parseFloat(amountToPay))
+                    }
+                  />
+                  <div className="p-3">
+                    <div className="space-x-3 flex justify-center items-center my-3">
+                      <Button
+                        onClick={handleReCookRice}
+                        disabled={reCooking || !active}
+                        loading={reCooking}
+                      >
+                        Re Plant
+                      </Button>
+                      <Button
+                        onClick={handleEatRice}
+                        disabled={eating || !active}
+                        loading={eating}
+                      >
+                        Harvest
+                      </Button>
+                    </div>
                   </div>
                 </div>
-              </div>
+              </Fragment>
             ) : (
               <div className="p-3 text-center flex flex-col items-center space-y-3 rounded-xl bg-white">
                 <p>You will need to connect your wallet first</p>
@@ -326,13 +349,13 @@ const MetricChip = ({
   borderColorClassName,
 }: MetricChipProps) => {
   return (
-    <div className="bg-white py-2 px-3 space-y-2 rounded-xl text-base">
+    <div className="bg-white py-2 px-3 space-y-1 rounded-xl text-base inline-block w-full">
       <div className="flex items-center text-xs space-x-1">
         <StaticImage
           alt=""
           src="../images/icon.png"
-          width={15}
-          height={15}
+          width={10}
+          height={10}
           placeholder="blurred"
         />
         <span>{symbol}</span>
@@ -343,7 +366,7 @@ const MetricChip = ({
         })}
       >
         <div className="font-medium">{label}</div>
-        <div className="font-light -mt-1">{value}</div>
+        <div className="font-medium text-gray-500">{value}</div>
       </div>
     </div>
   );
@@ -395,27 +418,43 @@ const TextInput = ({
   trx,
 }: TextInputProps) => {
   const hasError = errorMsg.length > 0;
+  const {
+    wallet: { balance },
+  } = useAppContext();
   return (
     <div className="w-full space-y-2 mx-auto">
       <div className="bg-white p-3 rounded-xl transition-transform duration-200 ease-linear">
         <div>
           <div className="mb-2 text-xs font-light">Deposit Amount</div>
-          <div className="relative flex items-center justify-between">
-            <input
-              type="text"
-              className={cls(
-                "placeholder-gray-400 outline-none border-b border-[#7B8BA5] font-bold",
-                "transition-all duration-200 text-[#7B8BA5] p-1 disabled:opacity-70 text-xl",
-                "disabled:cursor-not-allowed block w-full bg-transparent leading-none",
-                {
-                  "text-red-400": hasError,
-                }
-              )}
-              placeholder="0"
-              value={value}
-              onChange={onChangeHandler}
-            />
-            <div className="flex-none flex items-center space-x-1 text-base">
+          <div className="relative flex items-center justify-between space-x-1">
+            <div className=" w-full">
+              <input
+                type="text"
+                className={cls(
+                  "placeholder-gray-400 outline-none border-b border-[#7B8BA5] font-bold",
+                  "transition-all duration-200 text-[#7B8BA5] p-1 disabled:opacity-70 text-xl",
+                  "disabled:cursor-not-allowed block bg-transparent w-full leading-none",
+                  {
+                    "text-red-400": hasError,
+                  }
+                )}
+                placeholder="0"
+                value={value}
+                onChange={onChangeHandler}
+              />
+              <div
+                className={cls(
+                  "flex justify-between text-opacity-80 py-0.5 px-1 text-sm",
+                  {
+                    "text-red-400 font-normal": hasError,
+                  }
+                )}
+              >
+                <span>Balance</span>
+                <span>{hasError ? errorMsg : balance}</span>
+              </div>
+            </div>
+            <div className="flex items-center space-x-1 text-base">
               <StaticImage
                 src="../images/icon.png"
                 alt=""
@@ -426,9 +465,6 @@ const TextInput = ({
               <span>AVAX</span>
             </div>
           </div>
-        </div>
-        <div className={cls("text-sm", { ["text-red-400"]: hasError })}>
-          {errorMsg}
         </div>
       </div>
       <Button
